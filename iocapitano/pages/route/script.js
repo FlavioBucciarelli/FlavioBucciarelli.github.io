4e02c65897c5bc683f381f5d22bb3e70a69c468b
FlavioBucciarelli <116761654+FlavioBucciarelli@users.noreply.github.com>
2023-11-24 16:51:02 +0100

    Add files via upload

--- iocapitano/pages/route/script.js
+++ iocapitano/pages/route/script.js
@@ -1,3 +1,8 @@
+function edit_playback() {
+    const vid = document.getElementById("bg-vid")
+    vid.playbackRate = .8
+}
+
 window.onkeydown = () => {
     const section = document.getElementById("blur-section-2")
     section.classList.toggle("blur-section-2-show")
